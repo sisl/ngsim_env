421ba35ab0c870267e8d4a460b6388f00e1ae635
Derek <dphillips@tver.stanford.edu>
2018-04-06 17:02:31 -0700

    .

--- scripts/imitation/fix_experiments_rails.sh
+++ scripts/imitation/fix_experiments_rails.sh
@@ -1,6 +1,7 @@
 # Sometimes fine tune runs fail, so this is run to fix those. 
 
-python multiagent_curriculum_training.py --exp_name multiagent_rails_col_off_1_2_{} --reward_handler_use_env_rewards True &
+
+python imitate.py --env_multiagent True --use_infogail False --exp_name multiagent_rails_col_off_2000_1_50 --n_itr 200 --policy_recurrent True --n_envs 50 --params_filepath ../../data/experiments/multiagent_rails_col_off_2000_1_40/imitate/log/itr_200.npz --validator_render False --reward_handler_use_env_rewards True
 
 FAIL=0
 for job in `jobs -p`
@@ -11,14 +12,14 @@ done
 echo $FAIL
 
 # Now, FINE TUNE
-for num in 2; 
+for num in 1; 
 do
     model=${num}_fine
-    python imitate.py --exp_name multiagent_rails_col_off_1_$model --env_multiagent True \
+    python imitate.py --exp_name multiagent_rails_col_off_2000_$model --env_multiagent True \
         --use_infogail False --policy_recurrent True --n_itr 200 --n_envs 100 \
         --validator_render False  --batch_size 40000 --gradient_penalty 2 \
         --discount .99 --recurrent_hidden_dim 64 \
-        --params_filepath ../../data/experiments/multiagent_rails_col_off_1_${num}_50/imitate/log/itr_200.npz \
+        --params_filepath ../../data/experiments/multiagent_rails_col_off_2000_${num}_50/imitate/log/itr_200.npz \
         --reward_handler_use_env_rewards True &
 done
 
@@ -32,9 +33,9 @@ echo $FAIL
 
 # VALIDATE - creates the validation trajectories - simulates the model on each road section
 FAIL=0
-for model in 2_fine; 
+for model in 1_fine; 
 do
-    python validate.py --n_proc 20 --exp_dir ../../data/experiments/multiagent_rails_col_off_1_$model/ \
+    python validate.py --n_proc 20 --exp_dir ../../data/experiments/multiagent_rails_col_off_2000_$model/ \
         --params_filename itr_200.npz --use_multiagent True --random_seed 3 --n_envs 100 &
 
     for job in `jobs -p`
