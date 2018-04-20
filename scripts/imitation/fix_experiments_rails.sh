371587e09d551705e634beae90a54bed997dd71b
Derek <dphillips@tver.stanford.edu>
2018-04-20 11:19:51 -0700

    small viz (and run script) modifications

--- scripts/imitation/fix_experiments_rails.sh
+++ scripts/imitation/fix_experiments_rails.sh
@@ -1,7 +1,15 @@
 # Sometimes fine tune runs fail, so this is run to fix those. 
 
+###
+# RUN ALL CURRICULUM
+python multiagent_curriculum_training.py --exp_name multiagent_rails_col_off_5000_1_{} --reward_handler_use_env_rewards True &
 
-python imitate.py --env_multiagent True --use_infogail False --exp_name multiagent_rails_col_off_2000_1_50 --n_itr 200 --policy_recurrent True --n_envs 50 --params_filepath ../../data/experiments/multiagent_rails_col_off_2000_1_40/imitate/log/itr_200.npz --validator_render False --reward_handler_use_env_rewards True
+# OR  (EMPHASIS ON OR) RUN JUST SPECIFIC
+#python imitate.py --env_multiagent True --use_infogail False --exp_name multiagent_rails_col_off_5000_1_50 \
+#    --n_itr 200 --policy_recurrent True --n_envs 50 \
+#    --params_filepath ../../data/experiments/multiagent_rails_col_off_5000_1_40/imitate/log/itr_200.npz \
+#    --validator_render False --reward_handler_use_env_rewards True
+###
 
 FAIL=0
 for job in `jobs -p`
@@ -15,11 +23,11 @@ echo $FAIL
 for num in 1; 
 do
     model=${num}_fine
-    python imitate.py --exp_name multiagent_rails_col_off_2000_$model --env_multiagent True \
+    python imitate.py --exp_name multiagent_rails_col_off_5000_$model --env_multiagent True \
         --use_infogail False --policy_recurrent True --n_itr 200 --n_envs 100 \
         --validator_render False  --batch_size 40000 --gradient_penalty 2 \
         --discount .99 --recurrent_hidden_dim 64 \
-        --params_filepath ../../data/experiments/multiagent_rails_col_off_2000_${num}_50/imitate/log/itr_200.npz \
+        --params_filepath ../../data/experiments/multiagent_rails_col_off_5000_${num}_50/imitate/log/itr_200.npz \
         --reward_handler_use_env_rewards True &
 done
 
@@ -35,7 +43,7 @@ echo $FAIL
 FAIL=0
 for model in 1_fine; 
 do
-    python validate.py --n_proc 20 --exp_dir ../../data/experiments/multiagent_rails_col_off_2000_$model/ \
+    python validate.py --n_proc 20 --exp_dir ../../data/experiments/multiagent_rails_col_off_5000_$model/ \
         --params_filename itr_200.npz --use_multiagent True --random_seed 3 --n_envs 100 &
 
     for job in `jobs -p`
--- scripts/imitation/run_experiments_rails.sh
+++ scripts/imitation/run_experiments_rails.sh
@@ -2,13 +2,14 @@
 # They can all be run in parallel. After they are done, we will generate the validation trajectories.
 # There is also the fine tuning step in between.
 
-# RAILS - for right now just running with -1 reward for collision.
+# RAILS - specify reward augmentation in ngsim_env/julia/AutoEnvs/muliagent_ngsim_env.py, 
+#                                        function _extract_rewards()
 
-python multiagent_curriculum_training.py --exp_name multiagent_rails_col_off_2000_1_{} --reward_handler_use_env_rewards True &
+python multiagent_curriculum_training.py --exp_name multiagent_rails_col_off_+2000_1_{} --reward_handler_use_env_rewards True &
 
-python multiagent_curriculum_training.py --exp_name multiagent_rails_col_off_2000_2_{} --reward_handler_use_env_rewards True &
+python multiagent_curriculum_training.py --exp_name multiagent_rails_col_off_+2000_2_{} --reward_handler_use_env_rewards True &
 
-python multiagent_curriculum_training.py --exp_name multiagent_rails_col_off_2000_3_{} --reward_handler_use_env_rewards True &
+python multiagent_curriculum_training.py --exp_name multiagent_rails_col_off_+2000_3_{} --reward_handler_use_env_rewards True &
 
 FAIL=0
 for job in `jobs -p`
@@ -16,17 +17,17 @@ do
 	wait $job || let "FAIL+=1"
 done
 
-echo $FAIL
+echo "Failed : " $FAIL
 
 # Now, FINE TUNE
 for num in 1 2 3; 
 do
     model=${num}_fine
-    python imitate.py --exp_name multiagent_rails_col_off_2000_$model --env_multiagent True \
+    python imitate.py --exp_name multiagent_rails_col_off_+2000_$model --env_multiagent True \
         --use_infogail False --policy_recurrent True --n_itr 200 --n_envs 100 \
         --validator_render False  --batch_size 40000 --gradient_penalty 2 \
         --discount .99 --recurrent_hidden_dim 64 \
-        --params_filepath ../../data/experiments/multiagent_rails_col_off_2000_${num}_50/imitate/log/itr_200.npz \
+        --params_filepath ../../data/experiments/multiagent_rails_col_off_+2000_${num}_50/imitate/log/itr_200.npz \
         --reward_handler_use_env_rewards True &
 done
 
@@ -36,13 +37,13 @@ do
 	wait $job || let "FAIL+=1"
 done
 
-echo $FAIL
+echo "Failed : " $FAIL
 
 # VALIDATE - creates the validation trajectories - simulates the model on each road section
 FAIL=0
 for model in 1_fine 2_fine 3_fine; 
 do
-    python validate.py --n_proc 20 --exp_dir ../../data/experiments/multiagent_rails_col_off_2000_$model/ \
+    python validate.py --n_proc 20 --exp_dir ../../data/experiments/multiagent_rails_col_off_+2000_$model/ \
         --params_filename itr_200.npz --use_multiagent True --random_seed 3 --n_envs 100 &
 
     for job in `jobs -p`
@@ -51,6 +52,7 @@ do
         wait $job || let "FAIL+=1"
     done
 done
+echo "Failed : " $FAIL
 
 #Now that validation is done, there will be .npz trajectory files for each of the experiments
 # These should appear in ../../data/experiments/{model_name}/imiate/validation/
