43ae2f915f0d97b0a24dd8ea50578842ce5b57c2
Raunak Bhattacharyya <raunak@bombay>
2019-04-14 17:05:56 -0700

    Within notebook video using Reel

--- notebooks/visualization_functions.jl
+++ notebooks/visualization_functions.jl
@@ -1,3 +1,4 @@
+# TODO: How do you write tests for these visualization functions?
 # Define the overlay used to overlay the ground truth in white
 struct veh_overlay <: SceneOverlay
     sim_scene::Scene # The simulated scene
@@ -41,11 +42,50 @@ rec_sim = generate_truth_data(lane_place_array,car_particle_array)
 
 compare_trajectories(rec,rec_sim)
 """
-function compare_trajectories(rec_true,rec_sim)
+function compare_trajectories(rec_true,rec_sim,roadway)
     @assert nframes(rec_sim) == nframes(rec_true)
     @manipulate for frame_index in 1:nframes(rec_sim)
         ghost_overlay = veh_overlay(rec_true[frame_index-nframes(rec_sim)])
         render(rec_sim[frame_index-nframes(rec_sim)], roadway, [ghost_overlay], canvas_height=100)
     end
 end
-# TODO: How do you test the compare_trajectories function?
+
+"""
+    estimate_then_make_video(num_p::Int64,n_cars::Int64,lane_place_array::Array,
+        car_particles::Array,particle_props::Array;approach="pf")
+
+Estimate parameters of IDM, then use the mean of the particle bucket for each car to generate
+simulated trajectory. Make a video showing this simulated trajectory with ground truth
+overlayed in white
+
+# Other notable functions used
+- `find_mean_particles_carwise`
+- `compare_trajectories`
+"""
+function estimate_then_make_video(num_p::Int64,n_cars::Int64,lane_place_array::Array,
+        car_particles::Array,particle_props::Array;approach="pf")
+    scene,roadway = init_place_cars(lane_place_array)
+    rec = generate_truth_data(lane_place_array,car_particles)
+    f_end_num = length(rec.frames)
+    
+    bucket_array = initialize_carwise_particle_buckets(n_cars,num_p,particle_props)
+    for t in 1:f_end_num-1
+        #if t%10==0 @show t end
+        f = rec.frames[f_end_num - t + 1]
+
+        for car_id in 1:n_cars
+            old_p_set_dict = bucket_array[car_id]
+            trupos = rec.frames[f_end_num-t].entities[car_id].state.posF.s
+            new_p_set_dict = update_p_one_step(roadway,f,trupos,old_p_set_dict,
+                car_id=car_id,approach=approach)
+            bucket_array[car_id] = new_p_set_dict
+        end
+    end  
+    
+    # Estimation of parameters ends here. Now generate simulation
+    car_particles_sim = find_mean_particle_carwise(bucket_array)
+    sim_rec = generate_truth_data(lane_place_array,car_particles_sim)
+    
+    # Make a video with simulation in color and truth in white
+    compare_trajectories(rec,sim_rec,roadway)
+end
